cf604a13e24d69f2e5d3eb88d6608099a2b52fe5
Nate Cottle <cottle.nate@gmail.com>
2018-07-20 14:06:56 -0700

    Add passing tests for the store edit path in integration psecs.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -25,4 +25,31 @@ context 'store edit path' do
     click_link('Nike')
     expect(page).to have_content('Nike')
   end
+  it 'will go to the edit page' do
+    visit '/'
+    fill_in('store_name', :with => 'Nike')
+    click_button('Add Store')
+    click_link('Nike')
+    click_link('Edit')
+    expect(page).to have_content('Edit Nike')
+  end
+  it "will update store name" do
+    visit '/'
+    fill_in('store_name', :with => 'Nike')
+    click_button('Add Store')
+    click_link('Nike')
+    click_link('Edit')
+    fill_in('new_store_name', :with => 'Target')
+    click_button('Update')
+    expect(page).to have_content("Edit Target")
+  end
+  it 'will delete the store' do
+    visit '/'
+    fill_in('store_name', :with => 'Nike')
+    click_button('Add Store')
+    click_link('Nike')
+    click_link('Edit')
+    click_button('Delete')
+    expect(page).to have_no_content('Nike')
+  end
 end

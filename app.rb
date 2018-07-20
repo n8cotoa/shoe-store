167ec7c688f0cf15fef46a9e54eee07da0113b56
Nate Cottle <cottle.nate@gmail.com>
2018-07-20 09:52:29 -0700

    Add starting routes and display functionality.

--- app.rb
+++ app.rb
@@ -5,5 +5,25 @@ require('pry')
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get '/' do
-  erb :index
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:index)
+end
+
+post '/stores/new' do
+  store_name = params.fetch('store_name')
+  new_store = Store.create({:name => store_name})
+  redirect back
+end
+
+post '/brands/new' do
+  brand_name = params.fetch('brand_name')
+  brand_price = params.fetch('brand_price')
+  new_brand = Brand.create({:name => brand_name, :price => brand_price})
+  redirect back
+end
+
+get '/stores/:id' do
+  @store = Store.find(params.fetch(:id))
+  erb(:store)
 end

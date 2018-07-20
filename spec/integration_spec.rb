2fd289b4a99f069e06e1d1027908144b3af395ce
Nate Cottle <cottle.nate@gmail.com>
2018-07-20 14:00:55 -0700

    Add two more passing integration spec tests.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,3 +8,21 @@ context 'store creation path' do
     expect(page).to have_content('Nike')
   end
 end
+context 'shoe creation path' do
+  it 'will fill in form and new shoe will display' do
+    visit '/'
+    fill_in('brand_name', :with => 'Jordans')
+    fill_in('brand_price', :with => '25')
+    click_button('Add Shoe')
+    expect(page).to have_content('Jordans')
+  end
+end
+context 'store edit path' do
+  it 'will open individual store page' do
+    visit '/'
+    fill_in('store_name', :with => 'Nike')
+    click_button('Add Store')
+    click_link('Nike')
+    expect(page).to have_content('Nike')
+  end
+end
